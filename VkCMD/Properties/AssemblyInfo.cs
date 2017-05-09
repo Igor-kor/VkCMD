b2fdf2fe75683375ac538f4c5428153338635872
igor <igor_korenovsk@mail.ru>
2017-05-10 01:45:13 +0300

    rename project

--- VkCMD/Properties/AssemblyInfo.cs
+++ VkCMD/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("управление пк через вк с_шарп")]
+[assembly: AssemblyTitle("VkCMD")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("управление пк через вк с_шарп")]
+[assembly: AssemblyProduct("VkCMD")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
